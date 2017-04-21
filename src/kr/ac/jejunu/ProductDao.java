1a32c201c7b56b6f0f0c787d772e77a1a86e5ea2
Jaehyeon Kim <replaykim123@gmail.com>
2017-04-21 18:05:32 +0900

    tag_14

--- src/kr/ac/jejunu/ProductDao.java
+++ src/kr/ac/jejunu/ProductDao.java
@@ -1,42 +1,57 @@
 package kr.ac.jejunu;
 
-import javax.sql.DataSource;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import java.sql.*;
 
 public class ProductDao {
-    JdbcContext jdbcContext;
+    JdbcTemplate jdbcTemplate;
 
     public Product get(Long id) throws ClassNotFoundException, SQLException {
         String sql = "select * from product where id = ?";
         Object[] params = new Object[]{id};
 
-        return jdbcContext.queryForObject(sql, params);
+        Product product1 = null;
+        try {
+            product1 =  jdbcTemplate.queryForObject(sql, params, (resultSet, i) -> {
+                Product product = new Product();
+                product.setId(resultSet.getLong("id"));
+                product.setTitle(resultSet.getString("title"));
+                product.setPrice(resultSet.getInt("price"));
+
+                return product;
+            });
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+
+        return product1;
     }
 
     public void add(Product product) throws ClassNotFoundException, SQLException {
         String sql = "INSERT INTO product VALUES (?,?,?)";
         Object[] params = new Object[]{product.getId(), product.getTitle(), product.getPrice()};
 
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
 
     }
 
     public void delete(Long id) throws SQLException {
         String sql = "DELETE FROM product WHERE id = ?";
         Object[] params = new Object[]{id};
-        jdbcContext.update(sql, params);
+
+        jdbcTemplate.update(sql, params);
     }
 
     public void update(Product product) throws SQLException {
         String sql = "update product set title = ?, price = ? where id = ?";
         Object[] params = new Object[]{product.getTitle(), product.getPrice(), product.getId()};
 
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
-
-
 }
